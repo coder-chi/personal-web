65f0dd01ecb6aec05f9c2406964ba53eb21541c1
coder-chi <zczy_vr@163.com>
2018-01-23 10:54:24 +0800

    mybatis 配置完毕

---
+++ personal web/src/main/java/com/zczy/personal/web/DataSourceConfig.java
@@ -0,0 +1,47 @@
+package com.zczy.personal.web;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+/**
+ * 利用@Value注入关键属性建立DataSource的bean
+ */
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String userName;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+
+    @Bean
+    public DataSource dataSource() throws Exception{
+        Class.forName("com.mysql.jdbc.Driver");
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        dataSource.setReadOnly(false);
+        dataSource.setConnectionTimeout(30000);
+        dataSource.setIdleTimeout(600000);
+        dataSource.setMaximumPoolSize(1800000);
+        dataSource.setMaximumPoolSize(15);
+        return dataSource;
+    }
+}
